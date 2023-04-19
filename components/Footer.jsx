0f87f8a702b3c6caa7493ac76f702d65f79eeb47
insider-avinash <gavandheavinash12@gmail.com>
2023-04-19 11:52:37 +0530

    footer added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import styles from "../styles/Footer.module.css";
+import Image from "next/image";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <Image src="/img/bg.png" fill alt="" />
+      </div>
+      <div className={styles.item}>
+        <div className={styles.card}>
+          <h2 className={styles.motto}>
+            OH YES, WE DID THE PIZZA, WELL BAKED SLICE OF PIZZA.
+          </h2>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>FIND YOUR RESTAURANT</h1>
+          <p className={styles.text}>
+            1654 R. DON Road #304. <br /> New York, 85022 <br />
+            061 453 533
+          </p>
+          <p className={styles.text}>
+            1654 R. DON Road #304. <br /> New York, 85022 <br />
+            061 453 533
+          </p>
+          <p className={styles.text}>
+            1654 R. DON Road #304. <br /> New York, 85022 <br />
+            061 453 533
+          </p>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>WORKING HOURS</h1>
+          <p className={styles.text}>
+            MONDAY UNTIL FRIDAY <br /> 9:00 - 22:00
+          </p>
+          <p className={styles.text}>
+            SATURDAY- SUNDAY <br /> 12:00 -24:00{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
